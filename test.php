c394be0e102c7c29ef945977b0b6e25938c1cc4c
rommel dalisay <rommelmdalisay@gmail.com>
2024-09-10 10:31:16 +0800

    form data

--- test.php
+++ test.php
@@ -1,16 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <?php
- print_r($_ENV);
-  ?>
-</body>
-
+<!DOCTYPE html PUBLIC
+     "-//W3C//DTD XHTML 1.0 Strict//EN"
+     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+ <html>
+ <head>
+ <title>Listing 10.4 An HTML Form with a 'select' Element</title>
+ </head>
+ <body>
+ <div>
+ <form action="test2.php" method="POST">
+ <p><input type="text" name="user" /></p>
+ <p>
+ <textarea name="address" rows="5" cols="40">
+</textarea>
+ </p>
+ <p>
+ <select name="products[]" multiple="multiple">
+ <option>Sonic Screwdriver</option>
+ <option>Tricorder</option>
+ <option>ORAC AI</option>
+ <option>HAL 2000</option>
+ </select>
+ </p>
+ <p><input type="submit" value="hit it!" /></p>
+ </form>
+ </div>
+ </body>
 </html>
