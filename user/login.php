33e593908cb25c0105e4130647861ac6a01f1f17
rommel dalisay <rommelmdalisay@gmail.com>
2024-09-24 10:55:20 +0800

    listener create

--- user/login.php
+++ user/login.php
@@ -12,13 +12,14 @@ if (isset($_POST['submit'])) {
     $email = trim($_POST['email']);
    
     $pass = hash('sha256', trim($_POST['password']));
-    $sql = "SELECT email FROM users WHERE email='$email' AND password='$pass' LIMIT 1";
+    $sql = "SELECT user_id, email FROM users WHERE email='$email' AND password='$pass' LIMIT 1";
 
     $result = mysqli_query($conn, $sql);
 
     if (mysqli_num_rows($result) === 1) {
         $row = mysqli_fetch_assoc($result);
         $_SESSION['email'] = $row['email'];
+        $_SESSION['user_id'] = $row['user_id'];
         header("Location: ../index.php");
     }
 }
