721f033cb180fe1ad0e7610319c225441e8f77ae
rommel dalisay <rommelmdalisay@gmail.com>
2024-09-24 09:17:33 +0800

    session

--- test.php
+++ test.php
@@ -1,33 +1,26 @@
-<!-- <!DOCTYPE html PUBLIC
-     "-//W3C//DTD XHTML 1.0 Strict//EN"
-     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<?php
+ session_start();
+ ?>
+ <!DOCTYPE html PUBLIC
+:   "-//W3C//DTD XHTML 1.0 Strict//EN"
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
  <html>
  <head>
- <title>Listing 10.4 An HTML Form with a 'select' Element</title>
+ <title>Listing 20.5 Accessing Session Elements</title>
  </head>
  <body>
  <div>
- <form action="test2.php" method="POST">
- <p><input type="text" name="user" /></p>
- <p>
- <textarea name="address" rows="5" cols="40">
-</textarea>
- </p>
- <p>
- <!-- <select name="products[]" multiple="multiple">
- <option>Sonic Screwdriver</option>
- <option>Tricorder</option>
- <option>ORAC AI</option>
- <option>HAL 2000</option>
- </select> -->
- 
-<input type="checkbox" name="products[]" value="Sonic Screwdriver" />Sonic Screwdriver<br/>
-<input type="checkbox" name="products[]" value="Tricorder" />Tricorder<br/>
-<input type="checkbox" name="products[]" value="ORAC AI" />ORAC AI<br/>
-<input type="checkbox" name="products[]" value="HAL 2000" />HAL 2000<br/>
- </p>
- <p><input type="submit" value="hit it!" /></p>
- </form>
+ <h1>A Content Page</h1>
+ <?php
+ if ( is_array( $_SESSION['products'] ) ) {
+   print "<b>Your cart:</b><ol>\n";
+   foreach ( $_SESSION['products'] as $p ) {
+     print "<li>$p</li>";
+   }
+   print "</ol>";
+ }
+ ?>
+ <a href="test4.php">Back to product choice page</a>
  </div>
  </body>
-</html> -->
+ </html>
