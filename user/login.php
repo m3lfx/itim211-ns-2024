a7f7cc7d344fb35654f45b224f2e43e8a7e97b29
rommel dalisay <rommelmdalisay@gmail.com>
2024-09-24 08:53:30 +0800

    login form

---
+++ user/login.php
@@ -0,0 +1,53 @@
+<?php
+
+include("../includes/header.php");
+include("../includes/config.php");
+
+if (isset($_POST['submit'])) {
+    $email = trim($_POST['email']);
+   
+    $pass = hash('sha256', trim($_POST['password']));
+    $sql = "SELECT * FROM users WHERE email='$email' AND password='$pass' LIMIT 1";
+
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) === 1) {
+        $row = mysqli_fetch_assoc($result);
+    }
+}
+?>
+<div class="row col-md-8 mx-auto ">
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+        <!-- Email input -->
+        <div class="form-outline mb-4">
+            <input type="email" id="form2Example1" class="form-control" name="email" />
+            <label class="form-label" for="form2Example1">Email address</label>
+        </div>
+
+        <!-- Password input -->
+        <div class="form-outline mb-4">
+            <input type="password" id="form2Example2" class="form-control" name="password" />
+            <label class="form-label" for="form2Example2">Password</label>
+        </div>
+
+        <!-- 2 column grid layout for inline styling -->
+        <div class="row mb-4">
+            <div class="col d-flex justify-content-center">
+
+                
+            </div>
+
+           
+        </div>
+
+        <!-- Submit button -->
+        <button type="submit" class="btn btn-primary btn-block mb-4" name="submit">Sign in</button>
+
+        <!-- Register buttons -->
+        <div class="text-center">
+            <p>Not a member? <a href="register.php">Register</a></p>
+           
+            
+        </div>
+    </form>
+</div>
